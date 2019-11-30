9b522ef33552145c7b3288a5091d109fc6a5c588
Bezerk-Coder-Nikhil <30868407+NIKHILDUGAR@users.noreply.github.com>
2019-11-30 12:11:37 +0530

    Update test-xml.bat

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,11 +1,13 @@
-SET PATH=%PATH%;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\Scripts
-del C:\skillvelocity\courses\devops\PyMicroservices\Source\python_tests_xml\*.*
-del C:\skillvelocity\courses\devops\PyMicroservices\Source\coverage.xml
-cd c:\skillvelocity\courses\devops\PyMicroservices\Source
-REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python runtests.py
-REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage run -m pytest && coverage xml -o coverage.xml
-REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage html -d coverage
+SET PATH=%PATH%;C:\Users\nikhi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7;C:\Users\nikhi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Scripts
+del C:\Users\nikhi\micro2\Source\python_tests_xml\*.*
+del C:\Users\nikhi\micro2\Source\coverage.xml
+cd C:\Users\nikhi\micro2\Source
+REM C:\Users\nikhi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\python runtests.py
+REM C:\Users\nikhi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\python -m coverage run -m pytest && coverage xml -o coverage.xml
+REM C:\Users\nikhi\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\python -m coverage html -d coverage
 python runtests.py
 python -m coverage run -m pytest && coverage xml -o coverage.xml
-copy C:\skillvelocity\courses\devops\PyMicroservices\Source\python_tests_xml\*.* C:\skillvelocity\courses\devops\PyMicroservices\python_tests_xml
-copy C:\skillvelocity\courses\devops\PyMicroservices\Source\coverage.xml C:\skillvelocity\courses\devops\PyMicroservices
+copy C:\Users\nikhi\micro2\Source\python_tests_xml\*.* C:\Users\nikhi\micro2\python_tests_xml
+copy C:\Users\nikhi\micro2\Source\coverage.xml C:\Users\nikhi\micro2
+del C:\Users\nikhi\micro2\Source\python_tests_xml\*.*
+del C:\Users\nikhi\micro2\Source\coverage.xml
