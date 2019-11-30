ef2a628e6b761eb8766cdcaf5276411bf7e4e84a
Bezerk-Coder-Nikhil <30868407+NIKHILDUGAR@users.noreply.github.com>
2019-11-30 11:12:36 +0530

    Update docker-image-build.bat

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -1,4 +1,5 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd C:\Users\nikhi\Desktop\padhai\college\CLOUD\PyMicroservices-master\PyMicroservices-master\Source
+SET BUILD_NUMBER=123
+SET IMAGE_NAME=gcr.io/myproject2-258611/gpymicro-image:%BUILD_NUMBER%
+cd C:\Users\nikhi\micro2\Source
 gcloud builds submit --tag %IMAGE_NAME% .
