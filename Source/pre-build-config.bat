e07f97957420771ea7d47df6bdfb8fa85737e680
Bezerk-Coder-Nikhil <30868407+NIKHILDUGAR@users.noreply.github.com>
2019-11-30 08:58:17 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,3 +1,3 @@
 SET PATH=C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-git pull 
+C:\Program Files\Git\cmd\git.exe pull 
 gcloud auth activate-service-account jenkins@myproject2.iam.gserviceaccount.com --key-file= D:\myproject2-20238c4f5518.json --project=myproject2
