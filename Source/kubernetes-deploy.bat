5be56baaadcd2e4b344beabdf853c69a05fc7166
NIKHILDUGAR <nikhil4709@gmail.com>
2019-11-30 15:02:54 +0530

    Change Buildno

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET IMAGE_NAME=gcr.io/myproject2-258611/gpymicro-image:124
+SET IMAGE_NAME=gcr.io/myproject2-258611/gpymicro-image:%BUILD_NUMBER%
 cd C:\Users\nikhi\micro2\Source
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
